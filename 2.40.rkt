ead0f01f34f41c989e0f3ea33ada32fd4a6f42d8
Aleksey Lebedev <a.s.lebedev@fun-box.ru>
2023-05-11 23:33:06 +1000

    Add 2.40

---
+++ 2.40.rkt
@@ -0,0 +1,47 @@
+#lang racket
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+
+(define (find-pair pairs pair)
+  (if (empty? pairs)
+      #f
+      (let ([first (car pair)][second (cadr pair)])
+        (not
+         (empty?
+          (filter
+           (lambda (p) (and (= (car p) first) (= (cadr p) second)))
+           pairs))))))
+
+
+(define (unique-pairs n)
+  (let ([pairs
+         (flatmap
+          (lambda (i)
+            (map (lambda (j) (list i j))
+                 (range 1 (- i 1))))
+          (range 1 n))])
+    (map make-pair-sum (filter pair-sum-prime? pairs))))
+
+
+(define (prime? n)
+  (define (test divisor)
+    (cond ((>  (* divisor divisor) n) #t)
+          ((= (remainder n divisor) 0) #f)
+          (else (test (+ divisor 1)))))
+  (test 2))
+
+(define (pair-sum-prime? p)
+  (prime? (+ (car p) (cadr p))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(println (unique-pairs 11))
